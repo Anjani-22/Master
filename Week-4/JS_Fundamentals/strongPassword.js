4548979db3a7b23418619c8ad755288f26a62f1f
Anjani-22 <anjaniofficial22@gmail.com>
2023-11-05 10:07:25 +0530

    removed week folders

--- Week-4/JS_Fundamentals/strongPassword.js
+++
@@ -1,9 +0,0 @@
-function isStrongpassword(password)
-{
-    let l= password.length>=8 ? true : false;
-    let passKeyWord= password.toLowerCase().includes("password") ? false:true;
-    let caps=/[A-Z]+/.test(password) ;
-
-    return l && passKeyWord && caps;
-
-}
--- Week-6/Modules/index.js
+++
@@ -1,5 +0,0 @@
-import {add, subtract} from './calculator.js';
-
-
-console.log(add(45,48));
-console.log(add(45,48));
