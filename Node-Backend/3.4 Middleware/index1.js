153113c0c65b62fec59be8937b814f6afec89dd6
Anjani-22 <anjaniofficial22@gmail.com>
2023-11-06 15:47:18 +0530

    morgan

--- Node-Backend/3.4 Middleware/index1.js
+++ Node-Backend/3.4 Middleware/index1.js
@@ -1,13 +1,22 @@
 import express from "express";
-import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { dirname } from "path";
+import bodyParser from "body-parser";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true })); //express.urlencoded({ extended: true })
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+  console.log(req.body);
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
