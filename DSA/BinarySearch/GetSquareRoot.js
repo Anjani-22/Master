a1334cec87f667b5c7dac673bd19d17d0e3acdd1
Anjani-22 <anjaniofficial22@gmail.com>
2023-11-08 13:02:18 +0530

    added mid*mid===square target condition;

--- DSA/BinarySearch/GetSquareRoot.js
+++ DSA/BinarySearch/GetSquareRoot.js
@@ -3,7 +3,8 @@ function getSquareRoot(target) {
 }
 
 function helper(l, h, target) {
-  const mid = Math.floor(l + h - h / 2);
+  const mid = l + h - h / 2;
   if (mid * mid > target) return helper(l, mid + 1, target); //left search
+  else if (mid * mid === target) return mid;
   else return helper(mid + 1, h, target); //right search
 }
